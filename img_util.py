6d11e388e9d6a2e87edf2f16914f79113426ae08
odgiiv <odgiiv.kh@gmail.com>
2019-02-19 15:25:48 +0100

    add random crop to training images

--- img_util.py
+++ img_util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import cv2
 
 
@@ -43,3 +44,26 @@ def rotate_box(corners, angle, cx, cy, h, w):
     calculated = np.dot(M, corners.T).T
     calculated = calculated.reshape(-1, 8)
     return calculated
+
+
+def random_crop(image, poly_bboxes):
+    """
+    poly_bboxes has shape of (n,4,2) where n number of texts on the image (usually n=1).
+    Each bounding box has 4 (x1,y1,x2,y2,x3,y3,x4,y4) points with 2 values.
+    """
+    (h, w) = image.shape[:2]
+    poly_bboxes = poly_bboxes.reshape(-1, 2)
+    xmax, ymax = poly_bboxes.max(axis=0)
+    xmin, ymin = poly_bboxes.min(axis=0)
+    xmin_crop_at = random.randint(0, xmin)
+    xmax_crop_at = random.randint(xmax, w)
+
+    ymin_crop_at = random.randint(0, ymin)
+    ymax_crop_at = random.randint(ymax, w)
+
+    cropped_image = image[ymin_crop_at:ymax_crop_at, xmin_crop_at:xmax_crop_at]
+    new_corners = np.column_stack(
+        (poly_bboxes[:, 0] - xmin_crop_at, poly_bboxes[:, 1] - ymin_crop_at))
+
+    # reshape corners back to original shape
+    return (cropped_image, new_corners.reshape(-1, 4, 2))
